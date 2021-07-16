5454b130e91615b519bd80375323df1538cac594
minho-jang <minho9301@naver.com>
2021-07-16 16:32:05 +0900

    Chore: Test backend rendering in server

--- i-love-this-page-backend/src/main/java/com/minhojang/ilovethispagebackend/controller/MainController.java
+++ i-love-this-page-backend/src/main/java/com/minhojang/ilovethispagebackend/controller/MainController.java
@@ -1,14 +1,21 @@
 package com.minhojang.ilovethispagebackend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@CrossOrigin
 public class MainController {
 
+  static int likes = 30;
+
   @GetMapping("/")
-  public String helloWorld() {
-    return "Hello World";
+  public String index(Model model) {
+    model.addAttribute("likes", likes++);
+    return "index";
   }
 
 }
