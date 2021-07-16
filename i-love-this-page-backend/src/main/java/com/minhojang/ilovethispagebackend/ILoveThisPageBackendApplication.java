cbcee5c5ddd94f38e255ac209800bc40116d1da6
minho-jang <minho9301@naver.com>
2021-07-16 09:34:37 +0900

    Feat: Generate Spring boot project
    - backend 프로젝트 생성에 따라 디렉터리 구조 변경
    - http://localhost:8080 >> Hello world

---
+++ i-love-this-page-backend/src/main/java/com/minhojang/ilovethispagebackend/ILoveThisPageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minhojang.ilovethispagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveThisPageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILoveThisPageBackendApplication.class, args);
+	}
+
+}
