f187599c0d3c5b396f52ba77d9e2683271523b4d
minho-jang <minho9301@naver.com>
2021-07-28 10:25:59 +0900

    Feat: implement util about JSON

---
+++ i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/utils/JsonUtils.java
@@ -0,0 +1,39 @@
+package com.minhojang.ilikethispagebackend.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.minhojang.ilikethispagebackend.errors.JsonException;
+
+import java.util.Map;
+
+public class JsonUtils {
+
+  private static final ObjectMapper om = new ObjectMapper();
+
+  public static <T> T fromJson(String json, Class<T> type) {
+    try {
+      return om.readValue(json, type);
+    } catch (Exception e) {
+      throw new JsonException(e);
+    }
+  }
+
+  public static String toJson(Object obj) {
+    try {
+      return om.writeValueAsString(obj);
+    } catch (Exception e) {
+      throw new JsonException(e);
+    }
+  }
+
+  static final TypeReference<Map<String, Object>> typeOfMap = new TypeReference<Map<String, Object>>() {};
+
+  public static Map<String, Object> toMap(String json) {
+    try {
+      return om.readValue(json, typeOfMap);
+    } catch (Exception e) {
+      throw new JsonException(e);
+    }
+  }
+
+}
