9dbbf5cb76bbdf05d389f1963d891e53cde6e1ea
minho-jang <minho9301@naver.com>
2021-07-28 10:26:11 +0900

    Feat: implement util about String

---
+++ i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package com.minhojang.ilikethispagebackend.utils;
+
+public class StringUtils {
+
+  public static boolean isEmpty(String str) {
+    return str == null || str.length() == 0;
+  }
+
+  public static boolean isNotEmpty(String str) {
+    return !isEmpty(str);
+  }
+
+}
