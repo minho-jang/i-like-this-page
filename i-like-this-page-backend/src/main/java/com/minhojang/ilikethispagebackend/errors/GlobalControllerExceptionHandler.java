39db871463fb93139b35a893fe8eeecd6084cf59
minho-jang <minho9301@naver.com>
2021-07-30 09:32:59 +0900

    Feat: define GlobalControllerExceptionHandler

---
+++ i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/errors/GlobalControllerExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.minhojang.ilikethispagebackend.errors;
+
+import com.minhojang.ilikethispagebackend.api.ApiResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static com.minhojang.ilikethispagebackend.api.ApiResult.error;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalControllerExceptionHandler {
+
+  private ResponseEntity<ApiResult<?>> newResponse(Throwable throwable, HttpStatus status) {
+    return newResponse(throwable.getMessage(), status);
+  }
+
+  private ResponseEntity<ApiResult<?>> newResponse(String message, HttpStatus status) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    return new ResponseEntity<>(error(message, status), headers, status);
+  }
+
+  @ExceptionHandler({
+          JsonException.class,
+          InvalidArgumentException.class
+  })
+  public ResponseEntity<?> handleBadRequestException(Exception e) {
+    return newResponse(e, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<?> handleNotFoundException(Exception e) {
+    return newResponse(e, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler({
+          Exception.class,
+          RuntimeException.class
+  })
+  public ResponseEntity<?> handleException(Exception e) {
+    log.error("Unexpected exception occurred: {}", e.getMessage(), e);
+    return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+}
