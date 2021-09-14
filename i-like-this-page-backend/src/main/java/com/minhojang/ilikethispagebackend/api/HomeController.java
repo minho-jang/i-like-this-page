ad6e66a4de921a6e1083adaba7aff08ec54beb3f
minho-jang <minho9301@naver.com>
2021-09-14 23:17:26 +0900

    Fix: get application version automatically
    build.gradle에서 buildInfo()를 실행하면 BuildProperties 객체를 받을 수 있다. BuildProperties에서 버전 정보를 가져와 반환한다.

--- i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/api/HomeController.java
+++ i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/api/HomeController.java
@@ -1,7 +1,8 @@
 package com.minhojang.ilikethispagebackend.api;
 
-import com.minhojang.ilikethispagebackend.configures.LikeRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @Slf4j
 public class HomeController {
+	@Autowired
+	private BuildProperties buildProperties;
 
 	@GetMapping("/version")
-	public ApiResult<String> helloWorld() {
+	public ApiResult<String> version() {
 		return ApiResult.success(
-				"I LIKE THIS PAGE - 0.0.1v"
+				"I LIKE THIS PAGE - " + buildProperties.getVersion()
 		);
 	}
 }
