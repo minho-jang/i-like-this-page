f4b0aa7ae399623c46bbcda2c1e5a9bbd26e715f
minho-jang <minho9301@naver.com>
2021-09-16 14:55:36 +0900

    Fix: modify deploy log file path in script
    절대경로를 사용할 것!

--- i-like-this-page-backend/execute-deploy.sh
+++ i-like-this-page-backend/execute-deploy.sh
@@ -2,4 +2,4 @@
 
 touch /home/ubuntu/deploy.log
 
-/home/ubuntu/deploy.sh > deploy.log 2> deploy.log < /dev/null &
+/home/ubuntu/deploy.sh > /home/ubuntu/deploy.log 2> /home/ubuntu/deploy.log < /dev/null &
