0935ae7866e2bb3c2d269916a4c412592db0c0cb
minho-jang <minho9301@naver.com>
2021-09-16 14:22:57 +0900

    Fix: write deploy log
    deploy.log 파일을 생성하여 배포 출력문들을 기록한다

--- i-like-this-page-backend/deploy.sh
+++ i-like-this-page-backend/deploy.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-echo "> Checking pid of the running application..."
+touch deploy.log
+
+echo "> Checking pid of the running application..." > deploy.log
 
 CURRENT_PID=$(pgrep -f i-like-this-page)
 
-echo "PID = $CURRENT_PID"
+echo "PID = $CURRENT_PID" > deploy.log
 
 if [ -z $CURRENT_PID ]; then
-    echo "> There is no running application."
+    echo "> There is no running application." > deploy.log
 else
-    echo "> kill -15 $CURRENT_PID"
+    echo "> kill -15 $CURRENT_PID" > deploy.log
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
-echo "> Deploying a new application..."
+echo "> Deploying a new application..." > deploy.log
 
-echo "> Copy jar files"
+echo "> Copy jar files" > deploy.log
 
 cp build/libs/*.jar ../jar/
 
 JAR_NAME=$(ls -tr ../jar/ | grep 'i-like-this-page' | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME" > deploy.log
 
 nohup java -jar ../jar/$JAR_NAME &
