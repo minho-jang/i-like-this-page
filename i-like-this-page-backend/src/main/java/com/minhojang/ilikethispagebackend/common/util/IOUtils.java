492243738bf71d1729d2acbd6fa96f9f138fb921
minho-jang <minho9301@naver.com>
2021-09-30 23:10:21 +0900

    Feat: delivery javascript bundle file from backend

--- i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/common/util/IOUtils.java
+++ i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/common/util/IOUtils.java
@@ -1,6 +1,7 @@
 package com.minhojang.ilikethispagebackend.common.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
@@ -12,6 +13,10 @@ public class IOUtils {
 		return new BufferedReader(new InputStreamReader(input, charset))
 				.lines()
 				.collect(Collectors.joining("\n"));
+	}
+
+	public static byte[] toByteArray(InputStream in) throws IOException {
+		return in.readAllBytes();
 	}
 
 }
