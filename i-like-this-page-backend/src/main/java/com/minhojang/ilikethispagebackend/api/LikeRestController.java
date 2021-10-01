0f27029df1cdd22f408c13a4ff6946f72403e545
minho-jang <minho9301@naver.com>
2021-10-01 15:01:22 +0900

    Chore: rename MainRestController to LikeRestController

--- i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/api/LikeRestController.java
+++ i-like-this-page-backend/src/main/java/com/minhojang/ilikethispagebackend/api/LikeRestController.java
@@ -1,48 +1,42 @@
 package com.minhojang.ilikethispagebackend.api;
 
-import com.minhojang.ilikethispagebackend.common.dto.LikeResponseDto;
 import com.minhojang.ilikethispagebackend.common.dto.LikeRequestDto;
+import com.minhojang.ilikethispagebackend.common.dto.LikeResponseDto;
 import com.minhojang.ilikethispagebackend.service.LikeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/like")
 @CrossOrigin
 @Slf4j
-public class MainRestController {
+public class LikeRestController {
 
 	@Autowired
 	LikeService likeService;
 
-	@GetMapping("/like")
+	@GetMapping("")
 	public ApiResult<LikeResponseDto> getLike(LikeRequestDto param) {
-		log.info("Get 'like' count of url and Get 'like' status of user: " + param);
+		log.info("Get 'like' count of url and Get 'like' status of user with this ip address: " + param);
 		return ApiResult.success(
 				likeService.getLike(param.getUrl(), param.getUuid())
 		);
 	}
 
-	@PostMapping("/like")
+	@PostMapping("")
 	public ApiResult<LikeResponseDto> setLike(LikeRequestDto param) {
-		log.info("Add 'like' to this url: " + param);
+		log.info("Add 'like' to this url for this ip address: " + param);
 		return ApiResult.success(
 				likeService.saveLike(param.getUrl(), param.getUuid())
 		);
 	}
 
-	@DeleteMapping("/like")
+	@DeleteMapping("")
 	public ApiResult<LikeResponseDto> subtractLike(LikeRequestDto param) {
-		log.info("Cancel 'like' to this url: " + param);
+		log.info("Cancel 'like' to this url for this ip address: " + param);
 		return ApiResult.success(
 				likeService.deleteLike(param.getUrl(), param.getUuid())
 		);
-	}
-
-	@GetMapping("/check")
-	public ApiResult<String> checkToken() {
-		log.info("Check the token in cookie or Provide a token");
-		return ApiResult.success("checkToken not implemented");
 	}
 }
