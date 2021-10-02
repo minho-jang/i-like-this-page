20d2bb4ee47e2097321cb1445474e8c1aefe18a6
minho-jang <minho9301@naver.com>
2021-10-02 16:55:18 +0900

    Feat: bundle with webpack.config.js
    웹팩을 통해서 css도 함께 번들링한다.
    또한 babel 설정을 추가한다.
    js, css파일들을 src 디렉터리안에 모은다.

---
+++ i-like-this-page-frontend/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: "./src/js/i-like-this-page.js",
+  output: {
+    filename: "iltp.bundle.min.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
