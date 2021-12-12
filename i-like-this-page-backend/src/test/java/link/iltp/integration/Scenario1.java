ba2a07f8e16231f94db5a0d285e9e065214517d6
minho-jang <minho9301@naver.com>
2021-12-13 00:23:53 +0900

    fix: combine frontend CI step to with backend CI step
    백엔드에서 테스트로 JS 파일이 있는지 확인하기 때문에, 프론트엔드 빌드 결과물이 백엔드 폴더 안에 있어야 하므로, 프론트엔드 CI를 백엔드 CI와 합친다.
    
    통합 테스트 문자열을 조금 수정한다

--- i-like-this-page-backend/src/test/java/link/iltp/integration/Scenario1.java
+++ i-like-this-page-backend/src/test/java/link/iltp/integration/Scenario1.java
@@ -12,8 +12,8 @@ public class Scenario1 extends ScenarioBase {
 	private static final String SCENE_0 = "0. URL [" + TEST_URL + "]은 현재 3개의 좋아요를 가지고 있다. 사용자는 iltp 위젯을 사용하는 페이지에 들어가 본 적이 없다.";
 	private static final String SCENE_1 = "1. URL [" + TEST_URL + "]에 처음 접근하는 사용자는 로컬 스토리지에 토큰(iltp_tk)이 없다. 프론트엔드에서 토큰을 요청한다.";
 	private static final String SCENE_2 = "2. URL [" + TEST_URL + "]의 좋아요 개수를 가져온다.";
-	private static final String SCENE_3 = "3. URL [" + TEST_URL + "] iltp 위젯의 좋아요 버튼을 누른다.";
-	private static final String SCENE_4 = "4. URL [" + TEST_URL + "] iltp 위젯의 좋아요 버튼을 다시 누른다. 좋아요가 취소된다.";
+	private static final String SCENE_3 = "3. URL [" + TEST_URL + "]의 좋아요 버튼을 누른다.";
+	private static final String SCENE_4 = "4. URL [" + TEST_URL + "]의 좋아요 버튼을 다시 누른다. 좋아요가 취소된다.";
 	private static final String SCENE_END = "시나리오 #1 끝.";
 
 	private static String token = "";
